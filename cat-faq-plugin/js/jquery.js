f0b7febb61c1059c2a2c799b7be38d63fe22b69a
samier <samier.sompura@gmail.com>
2018-04-03 11:24:25 +0530

    design changes

--- cat-faq-plugin/js/jquery.js
+++ cat-faq-plugin/js/jquery.js
@@ -1,17 +1,23 @@
-jQuery(document).ready(function(){
-    jQuery('.accordion').on('click',function(){
+jQuery(document).ready(function () {
+    jQuery('.accordion').on('click', function () {
         jQuery(this).next().slideToggle().siblings('.answer').slideUp();
     });
 
     jQuery('#selectdrp').change(function () {
-        var getValue=jQuery(this).val();
- 
+        var getValue = jQuery(this).val();
+
         jQuery('.cat_all').hide();
-        jQuery('.cat_'+getValue).show();
+        jQuery('.cat_' + getValue).show();
     });
 
-    jQuery('#formid #searchid').change(function(){
+    jQuery('#formid #searchid').change(function () {
         jQuery('.cat_all').hide();
         jQuery('.searchresult').show();
     });
+
+    jQuery('body').delegate('.cat_2.cat_all', 'click', function () {
+        jQuery('.cat_2.cat_all').removeClass('faq_active');
+        jQuery(this).addClass('faq_active');
+    });
+    
 });
